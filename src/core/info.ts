f05cdb0aac5767b4c12d3e3c26094851639037a1
Marcos Lima Barbosa <Markin-work@protonmail.com>
2026-05-01 13:32:42 -0300

    Update LinkedIn URL in info object

--- src/core/info.ts
+++ src/core/info.ts
@@ -7,7 +7,7 @@ export const info = {
 	location: "Pernambuco - Brasil",
 	socialMedias: {
 		github: "https://github.com/MarcosLima-1",
-		linkedin: "https://www.linkedin.com/in/marcos-lima-56072734b",
+		linkedin: "https://www.linkedin.com/in/markinlima",
 		whatsApp: "https://wa.me/5587999631718",
 	},
 	phone: "87 999631718",
